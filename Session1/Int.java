2b4aa78c78ad9c6f579ee36b19f10cb08a6e1616
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2023-10-29 17:11:51 +0530

    Add files via upload

---
+++ Session1/Int.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.lang.Math;
+class Int {
+	public static void main(String args[]) {
+	byte x = 300;
+		//for(byte i=0; ; i++)
+				System.out.println(x);
+	}
+}
