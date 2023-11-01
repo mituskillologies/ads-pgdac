0b7517d9e63c003633b6e64ae4c8b299dbff15af
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2023-11-01 07:38:03 +0530

    Add files via upload

---
+++ Session4&5/SinglyLinkedList.java
@@ -0,0 +1,42 @@
+public class SinglyLinkedList {
+    private Node head;	
+    public static class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node currentNode = head;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+    }
+
+    public void printList() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.print(currentNode.data + " ");
+            currentNode = currentNode.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.addNode(10);
+        list.addNode(20);
+        list.addNode(30);
+
+        list.printList();
+    }
+}
