0cbf38b4324df61d91a0900adad869741573baae
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2023-11-04 14:23:06 +0530

    Add files via upload

---
+++ Session9,10,11/SelectionSort.java
@@ -0,0 +1,27 @@
+public class SelectionSort {
+
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 3, 2, 1, 4};
+
+        selectionSort(array);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
+
